f4af5ee20fc212269aec68b1f0f757f0eb756218
Wendy Alfaro <166208157+WenYanelly16@users.noreply.github.com>
2025-04-24 22:29:32 +0000

    Added newmessage functionality

--- TCP-vs-UDP/pkg/message.go
+++ TCP-vs-UDP/pkg/message.go
@@ -1,1 +1,24 @@
 //Filename: /pkg/message.go
+package pkg
+
+import (
+	"time"
+)
+
+type Message struct {
+	From    string
+	Content string
+	Time    time.Time
+}
+
+func NewMessage(from, content string) Message {
+	return Message{
+		From:    from,
+		Content: content,
+		Time:    time.Now(),
+	}
+}
+
+func (m Message) String() string {
+	return m.Time.Format("15:04:05") + " " + m.From + ": " + m.Content
+}
